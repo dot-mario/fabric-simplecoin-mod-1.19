dc92d7fb6a90d57534348c72b6742be4d527a609
kbh1933 <kbh1933@gmail.com>
2022-11-25 19:43:20 +0900

    Initial commit

---
+++ fabric-example-mod-1.19/src/main/java/com/dotmario/simplecoin/SimpleCoinModClient.java
@@ -0,0 +1,10 @@
+package com.dotmario.simplecoin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleCoinModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ fabric-example-mod-1.19/src/main/java/com/dotmario/simplecoin/item/ModItems.java
@@ -0,0 +1,4 @@
+package com.dotmario.simplecoin.item;
+
+public class ModItems {
+}
